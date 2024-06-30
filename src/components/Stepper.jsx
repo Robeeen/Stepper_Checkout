6bec239bb250009cf0e14e9953da6c02b4de2726
Robeeen <robeeen@gmail.com>
2024-06-30 19:55:45 +0600

    udpated

--- src/components/Stepper.jsx
+++ src/components/Stepper.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import '../index.css'
 
 const Stepper = ({stepConfig=[]}) => {
-   const [currentStep, setCureentStep]  = useState(3);
+   const [currentStep, setCureentStep]  = useState(1);
    const [isComplete, setIsComplete] = useState(false);
 
    if(!stepConfig.length){
@@ -10,30 +10,49 @@ const Stepper = ({stepConfig=[]}) => {
    }
 
    const handleNext = () => {
-
+       setCureentStep((prevStep) => {
+        if(prevStep === stepConfig.length){
+            setIsComplete(true);
+            return prevStep;
+        }else{
+            return prevStep + 1;
+        }
+       })
    }
+
+   const ActiveComponent = stepConfig[currentStep - 1].Components;
 
   return (
     <>
     <div className='main'>
         {stepConfig.map((step, index) => {
             return (
-            <div key={step.name} className={`'steps' ${currentStep > index + 1 || isComplete ? "complete" : ""}
-            ${currentStep === index + 1 ? "active" : ""}
+            <div key={step.name} 
+            className={`steps 
+                ${currentStep > index + 1 || isComplete ? "complete" : ""}
+                ${currentStep === index + 1 ? "active" : ""}
             `}
             >
-                <div className='step_number'>{ currentStep === index + 1 || isComplete ? (<span>&#10003;</span>) : (index + 1) }</div>
-                <div className='step_name'>{step.name}</div>
+                <div className='step_number'>
+                    { currentStep > index + 1 || isComplete ? (<span>&#10003;</span>) : (index + 1) }
+                </div>
+                <div className='step_name'>
+                    {step.name}
+                </div>
             </div>);
         })}
-    </div>  
-    {!isComplete && (
-        <button className='btn' onClick={handleNext}>
-            {currentStep === stepConfig.length? "Finish" : "Next"} 
-         </button>
-    )}
+    </div>    
+
+    <div className='bottom details'>
+        <ActiveComponent />
+            {!isComplete && (
+                <button className='btn' onClick={handleNext}>
+                    {currentStep === stepConfig.length ? "Finish" : "Next"} 
+                </button>
+            )}
+    </div>
     </> 
-  )
+  );
 }
 
 export default Stepper;
