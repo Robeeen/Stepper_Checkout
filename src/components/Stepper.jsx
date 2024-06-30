ec95e745efc70e2a72e913304195c6fd405bd0b5
Robeeen <robeeen@gmail.com>
2024-06-30 18:09:52 +0600

    udpated

--- src/components/Stepper.jsx
+++ src/components/Stepper.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../index.css'
 
 const Stepper = ({stepConfig=[]}) => {
+   const [currentStep, setCureentStep]  = useState(3);
+   const [isComplete, setIsComplete] = useState(false);
+
+   if(!stepConfig.length){
+    return <></>
+   }
+
+   const handleNext = () => {
+
+   }
+
   return (
+    <>
     <div className='main'>
         {stepConfig.map((step, index) => {
             return (
-            <div key={step.name} className='steps'>
-                <div className='step_number'>{index + 1}</div>
+            <div key={step.name} className={`'steps' ${currentStep > index + 1 || isComplete ? "complete" : ""}
+            ${currentStep === index + 1 ? "active" : ""}
+            `}
+            >
+                <div className='step_number'>{ currentStep === index + 1 || isComplete ? (<span>&#10003;</span>) : (index + 1) }</div>
                 <div className='step_name'>{step.name}</div>
             </div>);
         })}
-    </div>   
+    </div>  
+    {!isComplete && (
+        <button className='btn' onClick={handleNext}>
+            {currentStep === stepConfig.length? "Finish" : "Next"} 
+         </button>
+    )}
+    </> 
   )
 }
 
